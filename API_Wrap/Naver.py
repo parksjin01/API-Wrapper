662d9be315599de69c5624b44331c2a3ce0dc1c3
Damotorie <parksjin01@gmail.com>
2019-11-17 14:46:45 +0900

    [UPDATE] Implemenet `Naver Datalab Shopping` API wrapping method

--- API_Wrap/Naver.py
+++ API_Wrap/Naver.py
@@ -85,8 +85,72 @@ def datalab_search(startDate, endDate, timeUnit, keywordGroups, device=None, gen
 """
     URL: https://developers.naver.com/products/datalab/
 """
-def datalab_shopping():
-    """TODO"""
+def datalab_shopping(startDate, endDate, timeUnit, category, device=None, gender=None, ages=None):
+
+    if True:
+        if type(startDate) != str:
+            raise AttributeError("[ERROR] startDate parameter should be string with format yyyy-mm-dd")
+
+        if type(endDate) != str:
+            raise AttributeError("[ERROR] endDate parameter should be string with format yyyy-mm-dd")
+
+        if type(timeUnit) != str:
+            raise AttributeError("[ERROR] timeUnit parameter should be string")
+        elif timeUnit not in ["date", "week", "month"]:
+            raise AttributeError("[ERROR] timeUnit parameter should be one of [date / week / month]")
+
+        if type(category) != list:
+            raise AttributeError("[ERROR] category parameter should be list of dictionary {groupName: \"\", keywords: \"\"}")
+        if len(category) > 3:
+            raise AttributeError("[ERROR] Maximum length of category parameter is 3")
+        else:
+            for keyword in category:
+                if type(keyword) != dict:
+                    raise AttributeError("[ERROR] Element of category parameter should be dictionary")
+
+                try:
+                    if type(keyword["name"]) != str:
+                        raise AttributeError("[ERROR] Value of key `name` in category parameter should be string")
+                except:
+                    raise AttributeError("[ERROR] Key `name` is necessary in element of category parameter")
+
+                try:
+                    if type(keyword["param"]) != list:
+                        raise AttributeError("[ERROR] Value of key `param` in category parameter should be list of string")
+                    for k in keyword["param"]:
+                        if type(k) != str:
+                            raise AttributeError("[ERROR] Value in list of category.param should be string")
+                except:
+                    raise AttributeError("[ERROR] Key `param` is necessary in element of category parameter")
+
+        if device and type(device) != str:
+            raise AttributeError("[ERROR] device parameter should be string")
+
+        if gender and type(gender) != str:
+            raise AttributeError("[ERROR] gender parameter should be string")
+        elif gender and gender not in ["m", "f"]:
+            raise AttributeError("[ERROR] gender parameter should be one of [f / m]")
+
+        if ages and type(ages) != list:
+            raise AttributeError("[ERROR] ages parameter should be list of string")
+        elif ages:
+            for age in ages:
+                if type(age) != str:
+                    raise AttributeError("[ERROR] Element of ages parameter should be string")
+                if int(age) < 1 or int(age) > 10:
+                    raise AttributeError("[ERROR] Element value of ages parameter should be between 1 ~ 10")
+
+    client.set_header("Content-Type", "application/json")
+
+    posting_data = {
+        "startDate": startDate,
+        "endDate": endDate,
+        "timeUnit": timeUnit,
+        "category": category,
+    }
+
+    return client.post("https://openapi.naver.com/v1/datalab/shopping/categories", data=json.dumps(posting_data).encode("utf-8"))
+
 
 """
     URL: https://developers.naver.com/products/search/
