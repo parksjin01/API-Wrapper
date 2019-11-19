05537e128cf2619634d2e758e42bf4e8e95d77ea
Damotorie <parksjin01@gmail.com>
2019-11-19 21:13:34 +0900

    [UPDATE] Implement `Naver Search News` API wrapping method

--- API_Wrap/Naver.py
+++ API_Wrap/Naver.py
@@ -189,6 +189,43 @@ def searchingBlog(query, display=None, start=None, sort=None):
     return client.get("https://openapi.naver.com/v1/search/blog.json", params=getData)
 
 """
+    URL: https://developers.naver.com/products/search/
+"""
+def searchingNews(query, display=None, start=None, sort=None):
+
+    client.set_header("Accept", "*/*")
+
+    if type(query) != str:
+        raise AttributeError("[ERROR] query parameter should be string")
+
+    if display:
+        if type(display) != int:
+            raise AttributeError("[ERROR] display parameter should be int")
+        elif display < 1 or 100 < display:
+            raise AttributeError("[ERROR] display parameter value should be in 1 ~ 100")
+
+    if start:
+        if type(start) != int:
+            raise AttributeError("[ERROR] start parameter should be int")
+        elif start < 1 or 1000 < start:
+            raise AttributeError("[ERROR] start parameter value should be in 1 ~ 1,000")
+
+    if sort:
+        if type(sort) != str:
+            raise AttributeError("[ERROR] sort parameter should be str")
+        elif sort not in ["sim", "date"]:
+            raise AttributeError("[ERROR] sort parameter value should be [sim / date]")
+
+    getData = {
+        "query": query,
+        "display": display,
+        "start": start,
+        "sort": sort,
+    }
+
+    return client.get("https://openapi.naver.com/v1/search/news.json", params=getData)
+
+"""
     URL: https://developers.naver.com/products/shortenurl/
 """
 def shortenURL(url):
