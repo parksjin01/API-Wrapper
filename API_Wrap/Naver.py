f5aa73f83138959b28ccca77e21caf240d55d048
Damotorie <parksjin01@gmail.com>
2019-11-19 21:58:51 +0900

    [UPDATE] Update `Naver Search Web-page` API wrapping method

--- API_Wrap/Naver.py
+++ API_Wrap/Naver.py
@@ -571,6 +571,37 @@ def correctingTypo(query):
     return client.get("https://openapi.naver.com/v1/search/errata.json", params=getData)
 
 """
+    URL: https://developers.naver.com/products/search/
+"""
+def searchingWeb(query, display=None, start=None):
+
+    client.set_header("Accept", "*/*")
+
+    if type(query) != str:
+        raise AttributeError("[ERROR] query parameter should be string")
+
+    if display:
+        if type(display) != int:
+            raise AttributeError("[ERROR] display parameter should be int")
+        elif display < 1 or 100 < display:
+            raise AttributeError("[ERROR] display parameter value should be in 1 ~ 100")
+
+    if start:
+        if type(start) != int:
+            raise AttributeError("[ERROR] start parameter should be int")
+        elif start < 1 or 1000 < start:
+            raise AttributeError("[ERROR] start parameter value should be in 1 ~ 1,000")
+
+    getData = {
+        "query": query,
+        "display": display,
+        "start": start,
+    }
+
+    return client.get("https://openapi.naver.com/v1/search/webkr.json", params=getData)
+
+
+"""
     URL: https://developers.naver.com/products/shortenurl/
 """
 def shortenURL(url):
