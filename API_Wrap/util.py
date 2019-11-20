d389eae3b9c51191f0776f707b49068e0b4cfb33
Damotorie <parksjin01@gmail.com>
2019-11-20 20:53:27 +0900

    [UPDATE] Implement `Kakao Load Auth` method

--- API_Wrap/util.py
+++ API_Wrap/util.py
@@ -8,3 +8,9 @@ def naver_auth():
         auth_data = json.load(f)
 
     return auth_data["naver"]["client_id"], auth_data["naver"]["client_secret"]
+
+def kakao_auth():
+    with open(code_path + "/config/auth.json") as f:
+        auth_data = json.load(f)
+
+    return auth_data["kakao"]["native_app_key"], auth_data["kakao"]["rest_api_key"], auth_data["kakao"]["javascript_key"], auth_data["kakao"]["admin_key"]
