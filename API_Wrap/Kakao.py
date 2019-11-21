d64a9f2182d99a2dc33181facb1897fb9c1fb893
Damotorie <parksjin01@gmail.com>
2019-11-21 09:55:34 +0900

    [UPDATE] Implement `Kakao Search Tip` API wrapping method

--- API_Wrap/Kakao.py
+++ API_Wrap/Kakao.py
@@ -152,8 +152,37 @@ def searchingBlog(query, sort=None, page=None, size=None):
 """
     https://developers.kakao.com/docs/restapi/search
 """
-def searchingTIP():
-    """TODO"""
+def searchingTIP(query, sort=None, page=None, size=None):
+
+    if type(query) != str:
+        raise AttributeError("[ERROR] query parameter should be string type")
+
+    if sort:
+        if type(sort) != str:
+            raise AttributeError("[ERROR] sort parameter should be string type")
+        elif sort not in ["accuracy", "recency"]:
+            raise AttributeError("[ERROR] sort parameter should be one of [accuracy / recency]")
+
+    if page:
+        if type(page) != int:
+            raise AttributeError("[ERROR] page parameter should be int type")
+        elif page < 1 or 50 < page:
+            raise AttributeError("[ERROR] page parameter should be between 1 ~ 50")
+
+    if size:
+        if type(size) != int:
+            raise AttributeError("[ERROR] size parameter should be int type")
+        elif size < 1 or 50 < size:
+            raise AttributeError("[ERROR] size parameter should be between 1 ~ 50")
+
+    getData = {
+        "query": query,
+        "sort": sort,
+        "page": page,
+        "size": size
+    }
+
+    return client.get("https://dapi.kakao.com/v2/search/tip", params=getData)
 
 """
     https://developers.kakao.com/docs/restapi/search
