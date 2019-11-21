24cffe7d90838b132adfc47c350c2ac3772fb594
Damotorie <parksjin01@gmail.com>
2019-11-21 09:49:52 +0900

    [UPDATE] Implement `Kakao Search Web` API wrapping method

--- API_Wrap/Kakao.py
+++ API_Wrap/Kakao.py
@@ -1,1 +1,77 @@
-"""TODO"""
+from RestClient4py.client import RestClient
+from API_Wrap import util
+import json
+import re
+
+
+kakao_native_app_key, kakao_rest_api_key, kakao_javascript_key, kakao_admin_key = util.kakao_auth()
+client = RestClient()
+client.set_header("Authorization", "KakaoAK {}".format(kakao_rest_api_key))
+client.set_header("Accept", "*/*")
+
+"""
+    https://developers.kakao.com/docs/restapi/search
+"""
+def searchingWeb(query, sort=None, page=None, size=None):
+
+    if type(query) != str:
+        raise AttributeError("[ERROR] query parameter should be string type")
+
+    if sort:
+        if type(sort) != str:
+            raise AttributeError("[ERROR] sort parameter should be string type")
+        elif sort not in ["accuracy", "recency"]:
+            raise AttributeError("[ERROR] sort parameter should be one of [accuracy / recency]")
+
+    if page:
+        if type(page) != int:
+            raise AttributeError("[ERROR] page parameter should be int type")
+        elif page < 1 or 50 < page:
+            raise AttributeError("[ERROR] page parameter should be between 1 ~ 50")
+
+    if size:
+        if type(size) != int:
+            raise AttributeError("[ERROR] size parameter should be int type")
+        elif size < 1 or 50 < size:
+            raise AttributeError("[ERROR] size parameter should be between 1 ~ 50")
+
+    getData = {
+        "query": query,
+        "sort": sort,
+        "page": page,
+        "size": size
+    }
+
+    return client.get("https://dapi.kakao.com/v2/search/web", params=getData)
+
+
+
+"""
+    https://developers.kakao.com/docs/restapi/search
+"""
+def searchingImage():
+    """TODO"""
+
+"""
+    https://developers.kakao.com/docs/restapi/search
+"""
+def searchingBlog():
+    """TODO"""
+
+"""
+    https://developers.kakao.com/docs/restapi/search
+"""
+def searchingTIP():
+    """TODO"""
+
+"""
+    https://developers.kakao.com/docs/restapi/search
+"""
+def searchingBooks():
+    """TODO"""
+
+"""
+    https://developers.kakao.com/docs/restapi/search
+"""
+def searchingCafe():
+    """TODO"""
