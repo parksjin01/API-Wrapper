89ca3d71ff6fb29e2505ceedfb585ee3586717da
Damotorie <parksjin01@gmail.com>
2019-11-21 09:50:04 +0900

    [UPDATE] Implement `Kakao Search Video` API wrapping method

--- API_Wrap/Kakao.py
+++ API_Wrap/Kakao.py
@@ -44,7 +44,40 @@ def searchingWeb(query, sort=None, page=None, size=None):
 
     return client.get("https://dapi.kakao.com/v2/search/web", params=getData)
 
+"""
+    https://developers.kakao.com/docs/restapi/search
+"""
+def searchingVideo(query, sort=None, page=None, size=None):
 
+    if type(query) != str:
+        raise AttributeError("[ERROR] query parameter should be string type")
+
+    if sort:
+        if type(sort) != str:
+            raise AttributeError("[ERROR] sort parameter should be string type")
+        elif sort not in ["accuracy", "recency"]:
+            raise AttributeError("[ERROR] sort parameter should be one of [accuracy / recency]")
+
+    if page:
+        if type(page) != int:
+            raise AttributeError("[ERROR] page parameter should be int type")
+        elif page < 1 or 15 < page:
+            raise AttributeError("[ERROR] page parameter should be between 1 ~ 50")
+
+    if size:
+        if type(size) != int:
+            raise AttributeError("[ERROR] size parameter should be int type")
+        elif size < 1 or 30 < size:
+            raise AttributeError("[ERROR] size parameter should be between 1 ~ 50")
+
+    getData = {
+        "query": query,
+        "sort": sort,
+        "page": page,
+        "size": size
+    }
+
+    return client.get("https://dapi.kakao.com/v2/search/vclip", params=getData)
 
 """
     https://developers.kakao.com/docs/restapi/search
