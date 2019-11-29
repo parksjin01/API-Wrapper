a71c8ad50d8860a73538e60dafad183a2ddbd467
Damotorie <parksjin01@gmail.com>
2019-11-29 14:10:58 +0900

    [UPDATE] Youtube authentication method

--- API_Wrap/util.py
+++ API_Wrap/util.py
@@ -14,3 +14,9 @@ def kakao_auth():
         auth_data = json.load(f)
 
     return auth_data["kakao"]["native_app_key"], auth_data["kakao"]["rest_api_key"], auth_data["kakao"]["javascript_key"], auth_data["kakao"]["admin_key"]
+
+def youtube_auth():
+    with open(code_path + "/config/auth.json") as f:
+        auth_data = json.load(f)
+
+    return auth_data["youtube"]["api_key"]
